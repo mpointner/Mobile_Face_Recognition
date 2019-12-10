1732a5f5cd8ee36b289e20d7cab8d1f090b0c38f
Michael Pointner <1427791@gmail.com>
2019-12-10 19:10:03 +0100

    Android Video Preview Activity added

--- Android/app/src/main/java/at/ac/tuwien/pointner/cvsp_mobile_face_recognition/MainActivity.java
+++ Android/app/src/main/java/at/ac/tuwien/pointner/cvsp_mobile_face_recognition/MainActivity.java
@@ -1,8 +1,11 @@
 package at.ac.tuwien.pointner.cvsp_mobile_face_recognition;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -31,15 +34,61 @@ public class MainActivity extends AppCompatActivity {
 
     String currentPhotoPath;
 
+    private FloatingActionButton nextButton;
+
+    static int step = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
+        nextButton = (FloatingActionButton) findViewById(R.id.next);
     }
 
-    public void takeIDPhoto(View view)
-    {
+    public void next(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void action(View view) {
+        takeIDPhoto();
+        //showVideo();
+    }
+
+    private void showVideo() {
+        boolean hasCameraHardware = checkCameraHardware(this);
+        System.out.println("hasCameraHardware: "+hasCameraHardware);
+        System.out.println("NumberOfCameras: "+Camera.getNumberOfCameras());
+        Camera camera = getCameraInstance();
+        System.out.println("Camera: "+camera);
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            System.err.println("Camera is not available (in use or does not exist)");
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void takeIDPhoto() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -90,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
              */
             galleryAddPic();
             setPic();
+            nextButton.setVisibility(View.VISIBLE);
         }
     }
 
